9da24dd49266c9a7646b885fb9adc66759b04017
Gabriel Vieira <gabrielvieira1@protonmail.com>
2025-05-26 16:41:02 -0300

    Adiciona mensagens de erros

--- CameShop/Errors/DomainErrors.cs
+++ CameShop/Errors/DomainErrors.cs
@@ -24,20 +24,22 @@ namespace Cameshop.Errors
       public static readonly DomainError NotFound = new(1001, "Usuário não encontrado.");
       public static readonly DomainError EmailInUse = new(1002, "O e-mail informado já está em uso.");
       public static readonly DomainError InvalidLogin = new(1003, "E-mail ou senha incorretos.");
+      public static readonly DomainError InvalidCredentials = new(1003, "Credenciais inválidas.");
+      public static readonly DomainError Unauthorized = new(1004, "Acesso não autorizado.");
     }
 
     public static class Validation
     {
-      public static readonly DomainError InvalidEmail = new(2001, "E-mail inválido.");
-      public static readonly DomainError InvalidPassword = new(2002, "Senha inválida.");
-      public static readonly DomainError InvalidName = new(2003, "Nome inválido." );
+      public static readonly DomainError InvalidEmail = new(2001, "E-mail inválido. Informe um endereço de e-mail válido no formato exemplo@dominio.com.");
+      public static readonly DomainError InvalidPassword = new(2002, "Senha inválida. A senha deve conter letra maiúscula e minúscula, número e caractere especial.");
+      public static readonly DomainError InvalidName = new(2003, "Nome inválido. O nome deve conter apenas letras maiúscula e minúscula");
       public static readonly DomainError InvalidCredentials = new(2004, "As credenciais fornecidas são inválidas.");
     }
 
     public static class System
     {
       public static readonly DomainError ErrorUserRegister = new(9001, "Erro ao registrar usuário.");
-      public static readonly DomainError ErrorLogin = new(9003, "Erro ao tentar realizar login."  );
+      public static readonly DomainError ErrorLogin = new(9003, "Erro ao tentar realizar login.");
       public static readonly DomainError ErrorUserUpdate = new(9004, "Erro ao atualizar o usuário.");
       public static readonly DomainError UnexpectedError = new(9999, "Erro inesperado. Tente novamente.");
       public static readonly DomainError UserDeleted = new(9006, "Usuário deletado.");
@@ -50,7 +52,6 @@ namespace Cameshop.Errors
       public static readonly DomainError InvalidPrice = new(3004, "Preço do item inválido.");
       public static readonly DomainError InvalidCreatedItem = new(3005, "Erro ao validar criação de item.");
       public static readonly DomainError InvalidUpdatedeItem = new(3005, "Erro ao validar atualização de item.");
-      public static readonly DomainError UnexpectedError = new(3900, "Erro inesperado ao processar item.");
     }
   }
 }
